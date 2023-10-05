5799f3b2ae89407f900d25bdcc27890b8e4e1fb6
Davide Dunne Sanchez <111002142+DavideDunne@users.noreply.github.com>
2023-10-05 11:01:41 -0600

    models

---
+++ Imageboard/Imageboard/Models/OriginalPoster.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Imageboard.Models
+{
+    public class OriginalPoster : Post
+    {
+        public char Board {  get; set; }
+    }
+}
---
+++ Imageboard/Imageboard/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Imageboard.Models
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+    }
+}
